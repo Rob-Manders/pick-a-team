157f852bdc57cad22a02a7aaff576967df2fe377
Robert Manders <robert.manders@gmail.com>
2023-03-21 21:03:39 +0000

    Fix bugs with logic.

--- src/components/PickTeamButton.tsx
+++ src/components/PickTeamButton.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components'
+import { AppContext } from '../context/AppContext'
 
 const Button = styled.button`
   background-color: red;
 `
 
 export default function PickTeamButton() {
-  return <Button>Pick a Team!</Button>
+  const { pickTeam } = useContext(AppContext)
+
+  return <Button onClick={pickTeam}>Pick a Team!</Button>
 }
--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -18,7 +18,7 @@ interface Value {
 export const AppContext = createContext<Value | undefined>(undefined)
 
 export default function AppContextProvider({ children }: Props) {
-  const [teamsAvailable, setTeamsAvailable] = useState<Team[]>(teams)
+  const [teamsAvailable, setTeamsAvailable] = useState<Team[]>([...teams])
   const [teamsGone, setTeamsGone] = useState<Team[]>([])
   const [selectedTeam, setSelectedTeam] = useState<Team | undefined>(undefined)
   const [madeFirstSelection, setMadeFirstSelection] = useState<boolean>(false)
@@ -26,26 +26,26 @@ export default function AppContextProvider({ children }: Props) {
   function pickTeam() {
     if (teamsAvailable.length > 0) {
       const teamIndex = Math.floor(Math.random() * teamsAvailable.length)
-      const selectedTeam = teams[teamIndex]
-
-      setSelectedTeam(selectedTeam)
+      const thisSelection = teamsAvailable[teamIndex]
 
       if (madeFirstSelection) {
-        setTeamsGone(prevState => {
-          return [selectedTeam, ...prevState]
-        })
-
+        setTeamsGone(prevState => [selectedTeam, ...prevState])
+      } else {
         setMadeFirstSelection(true)
       }
 
+      setSelectedTeam(thisSelection)
+
       setTeamsAvailable(prevState => {
-        return prevState.splice(teamIndex, 1)
+        const teamsRemaining = prevState
+        teamsRemaining.splice(teamIndex, 1)
+        return teamsRemaining
       })
     }
   }
 
   function reset() {
-    setTeamsAvailable(teams)
+    setTeamsAvailable([...teams])
     setTeamsGone([])
     setSelectedTeam(undefined)
     setMadeFirstSelection(false)
